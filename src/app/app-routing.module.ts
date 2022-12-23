b8588f1f095d79e65b2e434e0016d7b7611720e7
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2022-12-23 11:34:46 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OpenComponent } from './open/open.component';
+import { DoneComponent } from './done/done.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: AppComponent},
+  {path: 'open', component: OpenComponent},
+  {path: 'done', component: DoneComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpenComponent } from './open/open.component';
+import { DoneComponent } from './done/done.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenComponent,
+    DoneComponent
   ],
   imports: [
     BrowserModule,
